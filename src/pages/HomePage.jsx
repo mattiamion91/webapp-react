073dbf6f5805375094eb89c05f7d518d3c33ece1
Mattia Mion <mattia.mion@gmail.com>
2026-02-24 17:46:28 +0100

    import azios in home page func to handle be call

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,65 +1,29 @@
+//importo axios
+import axios from "axios"
+//importo state e effect
+import { useState, useEffect } from "react"
+
 import CardMovie from "../components/CardMovie"
 
-//dati temporanei per test props
-const movies = [
-    {
-        "id": 1,
-        "title": "Inception",
-        "director": "Christopher Nolan",
-        "genre": "Science Fiction",
-        "release_year": 2010,
-        "abstract": "A skilled thief is given a chance at redemption if he can successfully perform inception.",
-        "image": "inception.jpg",
-        "created_at": "2024-11-29T10:40:13.000Z",
-        "updated_at": "2025-05-22T10:55:27.000Z"
-    },
-    {
-        "id": 2,
-        "title": "The Godfather",
-        "director": "Francis Ford Coppola",
-        "genre": "Crime",
-        "release_year": 1972,
-        "abstract": "The story of a powerful Italian-American crime family and their struggles.",
-        "image": "interstellar.jpg",
-        "created_at": "2024-11-29T10:40:13.000Z",
-        "updated_at": "2025-05-22T10:55:27.000Z"
-    },
-    {
-        "id": 3,
-        "title": "Titanic",
-        "director": "James Cameron",
-        "genre": "Romance",
-        "release_year": 1997,
-        "abstract": "A romantic story set against the tragic sinking of the RMS Titanic.",
-        "image": "matrix.jpg",
-        "created_at": "2024-11-29T10:40:13.000Z",
-        "updated_at": "2025-05-22T10:55:27.000Z"
-    },
-    {
-        "id": 4,
-        "title": "The Matrix",
-        "director": "The Wachowskis",
-        "genre": "Action",
-        "release_year": 1999,
-        "abstract": "A hacker discovers the truth about his reality and his role in the war against its controllers.",
-        "image": "the_godfather.jpg",
-        "created_at": "2024-11-29T10:40:13.000Z",
-        "updated_at": "2025-05-22T10:55:27.000Z"
-    },
-    {
-        "id": 5,
-        "title": "Interstellar",
-        "director": "Christopher Nolan",
-        "genre": "Science Fiction",
-        "release_year": 2014,
-        "abstract": "A team of explorers travels through a wormhole in space to save humanity.",
-        "image": "titanic.jpg",
-        "created_at": "2024-11-29T10:40:13.000Z",
-        "updated_at": "2025-05-22T10:55:27.000Z"
-    }
-]
+//creo variabile per gestire endpoint
+const endpoint = "http://localhost:3000/api/movies";
 
 const HomePage = () => {
+
+    //imposo variabile di stato
+    const [movies, setMovies] = useState([]);
+
+    //funzione che gestisce chiamata a BE
+    const fetchMovie = () => {
+        axios.get(endpoint)
+        .then(res => {console.log(res.data);
+        })
+        .catch(err => {console.log(err);
+        });
+    };
+
+    //richamo funz di fetch al montaggio home page
+    useEffect(fetchMovie, []);
 
     //funzione rendering listato movie
     const renderMovies = () => {
