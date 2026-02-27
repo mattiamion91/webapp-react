8140f0363c06ea51337c78a8adee8fd9fdf8e604
Mattia Mion <mattia.mion@gmail.com>
2026-02-27 09:40:00 +0100

    fix

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -58,7 +58,7 @@ const MoviePage = () => {
                 {renderReviews()}
             </section>
             <section>
-                <RevForm book_id={book.id}/>
+                <RevForm movie_id={movie.id}/>
             </section>
             <footer className="border-top border-1 pt-2 mb-3 d-flex justify-content-end">
                 <Link className="btn btn-secondary" to="/">Back to home</Link>
